6d03202ba610b89936591d761ab745f376972e7b
Hilmi Fawwaz Sa'ad <128681954+hilmifawwazsaad@users.noreply.github.com>
2024-12-09 14:29:11 +0700

    fix: issue backend

--- backend/models/book.go
+++ backend/models/book.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
 type Book struct {
 	ID            uint          `gorm:"primaryKey" json:"id"`
 	CategoryID    uint          `gorm:"not null;constraint:OnDelete:RESTRICT;OnUpdate:CASCADE;" json:"category_id"`
@@ -9,7 +13,30 @@ type Book struct {
 	JumlahHalaman int           `gorm:"type:integer;not null" json:"jumlah_halaman"`
 	BahasaBuku    string        `gorm:"type:varchar(20);not null" json:"bahasa_buku"`
 	Stok          int           `gorm:"type:integer" json:"stok"`
+	Category      Category      `gorm:"foreignKey:CategoryID" json:"category"`
 	Transaction   []Transaction `gorm:"foreignKey:BookID" json:"transaction"`
 
 	Timestamp
+}
+
+func GetBookByID(db *gorm.DB, id int) (Book, error) {
+	var book Book
+	if err := db.First(&book, id).Error; err != nil {
+		return book, err
+	}
+	return book, nil
+}
+
+func UpdateBook(db *gorm.DB, book Book) error {
+	if err := db.Save(&book).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteBook(db *gorm.DB, id int) error {
+	if err := db.Delete(&Book{}, id).Error; err != nil {
+		return err
+	}
+	return nil
 }
--- backend/models/transaction.go
+++ backend/models/transaction.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+	"encoding/json"
+)
 
 type Transaction struct {
 	ID             uint      `gorm:"primaryKey" json:"id"`
@@ -8,6 +13,44 @@ type Transaction struct {
 	BookID         uint      `gorm:"not null;constraint:OnDelete:RESTRICT;OnUpdate:CASCADE;" json:"book_id"`
 	TanggalPinjam  time.Time `gorm:"type:date;not null" json:"tanggal_pinjam"`
 	TanggalKembali time.Time `gorm:"type:date;not null" json:"tanggal_kembali"`
+	Status         string    `gorm:"type:enum('Dipinjam','Dikembalikan');default:'Dipinjam'" json:"status"`
+	Book           Book      `gorm:"foreignKey:BookID" json:"book"`
+	User           User      `gorm:"foreignKey:UserID" json:"user"`
 
 	Timestamp
+}
+
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	type Alias Transaction
+	return json.Marshal(&struct {
+		TanggalPinjam  string `json:"tanggal_pinjam"`
+		TanggalKembali string `json:"tanggal_kembali"`
+		*Alias
+	}{
+		TanggalPinjam:  t.TanggalPinjam.Format("2006-01-02"),
+		TanggalKembali: t.TanggalKembali.Format("2006-01-02"),
+		Alias:          (*Alias)(t),
+	})
+}
+
+func GetTransactionByID(db *gorm.DB, id int) (Transaction, error) {
+	var transaction Transaction
+	if err := db.First(&transaction, id).Error; err != nil {
+		return transaction, err
+	}
+	return transaction, nil
+}
+
+func UpdateTransaction(db *gorm.DB, transaction Transaction) error {
+	if err := db.Save(&transaction).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteTransaction(db *gorm.DB, id int) error {
+	if err := db.Delete(&Transaction{}, id).Error; err != nil {
+		return err
+	}
+	return nil
 }
